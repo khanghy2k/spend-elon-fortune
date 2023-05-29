350e567234b791df24c07a580b7de63dca4db907
khanghy2k@gmail.com <khanghy2k@gmail.com>
2023-05-29 20:17:36 +0700

    update

---
+++ spend-elon-fortune/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Header from './component/Header';
+import ListItems from './component/ListItems';
+
+function App() {
+  return (
+    <div className="App">
+     <div className='App'>
+        <Header />
+        <ListItems />
+      </div>
+    </div>
+  );
+}
+
+export default App;
